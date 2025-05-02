78c369be3ec36be4454e3ac21273c1fc1fc3f511
Haz Developer <hazimshakri7@gmail.com>
2025-05-02 15:15:52 +0800

    feat: profile page

--- src/app/user-profile/page.tsx
+++ src/app/user-profile/page.tsx
@@ -1,10 +1,10 @@
 import BottomNavigation from "@/components/shared/bottombar/BottomNavigation";
+import Profile from "@/components/shared/profile/Profile";
 
 export default function UserProfile() {
     return (
-        <div className="flex flex-col min-h-screen bg-black">
-            <main className="flex flex-col items-center gap-6 flex-1 p-4 pt-8">
-            </main>
+        <div className="min-h-screen bg-black">
+            <Profile />
             <footer className="border-t p-4">
                 <BottomNavigation />
             </footer>
